ea350551dfc45b0178ce48a83dd729ff873593f7
Ignacio Martinez <agustinmartinezbarrios2@hotmail.com>
2023-09-06 00:49:19 -0300

    Base Page Settings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Header } from './components/Header';
+import { Landing } from './pages/Landing';
+import { NoMatch } from './pages/NoMatch';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
