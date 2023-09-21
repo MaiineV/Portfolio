e69dd6a400977452a3585bda09d11e29f7b62ca9
Ignacio Martinez <agustinmartinezbarrios2@hotmail.com>
2023-09-21 16:44:40 -0300

    Work Experience and header bugfixing

--- src/pages/WorkExperience.js
+++ src/pages/WorkExperience.js
@@ -4,21 +4,23 @@ import classes from "../styles/WorkExperience.module.scss";
 const WorkExperience = () => {
   return (
     <div className={classes.workExperiences}>
-      <div className={classes.workExperience__title}>WorkExperience</div>
-      <ul>
-        <li className={classes.workExperience____itemList}>
-          Full-stack Programmer - OWO de Culturales Hierbabuna SRL - 5 Month
-          (November 2022 - February 2023)
-        </li>
-        <li className={classes.workExperience____itemList}>
-        SQL Programmer / Database Manager  - FUSANA 
-- 4 Month (May 2022 - September 2022)
-        </li>
-        <li className={classes.workExperience____itemList}>
-        Assistant Proffesor and Proffesor - Escuela Davinci 
-- 1 Year (Agosto 2022 - Present)
-        </li>
-      </ul>
+      <div className={classes.workExperiences__title}>Work Experience</div>
+      <div className={classes.workExperiences__jobsList}>
+        <div className={classes.workExperiences__itemList}>
+          Position: Assistant Proffesor and Proffesor <br />
+          Company: Escuela Davinci <br />
+          Work Time: 1 Year (Agosto 2022 - Present)
+        </div>
+        <div className={classes.workExperiences__itemList}>
+          Position: Full-stack Programmer <br />
+          Company: OWO de Culturales Hierbabuna SRL <br />
+          Work Time: 5 Month (November 2022 - February 2023)
+        </div>
+        <div className={classes.workExperiences__itemList}>
+          Position: SQL Programmer / Database Manager <br />
+          Company: FUSANA <br />- 4 Work Time: Month (May 2022 - September 2022)
+        </div>
+      </div>
     </div>
   );
 };
